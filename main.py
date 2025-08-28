ae86740053b351213f8f8cf7fa63c5a87c98c4ec
3RRQR <errormielczarek2004@gmail.com>
2025-08-28 18:25:47 +0200

    added assets

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import tkinter as tk
+import random
+import sleep
 
 root = tk.Tk()
+face = random.randint(1, 30)
 
 # Setting some window properties
 root.title("BMO")
-root.configure(background="#49bc9f")
-root.minsize(200, 200)
-root.maxsize(500, 500)
-root.geometry("300x300+50+50")
+root.configure(background="#c9e4c3")
+root.geometry("1280x720+50+50")
 
 
-image = tk.PhotoImage(file="025.gif")
+image = tk.PhotoImage(file=f'assets/Rosto-{face}.png')
 tk.Label(root, image=image).pack()
 
 root.mainloop()
+
