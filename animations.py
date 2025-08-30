8f3fa20a0e316ed665f9c9b788b19d46296e0869
3RRQR <errormielczarek2004@gmail.com>
2025-08-30 14:03:58 +0200

    added animations.py

---
+++ animations.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+talking = [
+    tk.PhotoImage(file=f'assets/Rosto-1.png'),
+    tk.PhotoImage(file=f'assets/Rosto-2.png')
+]
+
+talkdur = [
+    50,
+    1000
+]
+
+blinking = [
+    tk.PhotoImage(file=f'assets/Rosto-4.png'),
+    tk.PhotoImage(file=f'assets/Rosto-1.png')
+]
+
+blinkdur = [
+    50,
+    1000
+]
--- main.py
+++ main.py
@@ -2,27 +2,25 @@ import tkinter as tk
 import random
 
 root = tk.Tk()
-talk = 1
 
-frames = [
-tk.PhotoImage(file=f'assets/Rosto-1.png'),
-tk.PhotoImage(file=f'assets/Rosto-2.png')
-]
+import animations as anim
 
 # Setting some window properties
 root.title("BMO")
 root.configure(background="#c9e4c3")
 root.geometry("1280x720+50+50")
 
-
-label = tk.Label(root, image=frames[0])
+label = tk.Label(root, image=anim.talking[0])
 label.pack()
 
-def talking(frame=0):
-    label.config(image=frames[frame])
-    root.after(random.randint(100, 500), talking, (frame+1) % len(frames))
+def animation(mood, dur, frame=0):
+    label.config(image=mood[frame])
+    if frame==0:
+        root.after(random.randint(dur[0], dur[1]), animation, mood, dur, (frame+1) % len(mood))
+    else:
+        root.after(dur[frame], animation, mood, dur, (frame+1) % len(mood))
 
-talking()
+animation(anim.blinking, anim.blinkdur)
 
 
 root.mainloop()
